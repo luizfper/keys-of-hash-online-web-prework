24318ed0fb9445246c2cbe6b283425b8fd553d17
Luiz Pereira <luizfper@gmail.com>
2019-02-05 15:51:41 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -19,4 +19,4 @@ end
 hashao={}
 hashao={"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
 #hashao={a: 1, b: 2, c: 3}
-hashao.keys_of("Madagascar")
+hashao.keys_of("Madagascar","Panama")
