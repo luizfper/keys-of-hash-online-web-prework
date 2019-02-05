8a222fb03995b1c00d29f12f0145c517dd8929f1
Luiz Pereira <luizfper@gmail.com>
2019-02-05 15:45:24 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -17,6 +17,6 @@ class Hash
 end
 
 hashao={}
-#hashao={"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
-hashao={a: 1, b: 2, c: 3}
-hashao.keys_of(2)
+hashao={"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
+#hashao={a: 1, b: 2, c: 3}
+hashao.keys_of("Madagascar")
