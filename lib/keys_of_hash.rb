4f9f3f8664801ee00d52d7b596e00c7b2a0deee4
Luiz Pereira <luizfper@gmail.com>
2019-02-05 15:55:29 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,18 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    arg_array=[]
+    arg_array=arguments
+    arr=[]
+    arg_array.each do |argument|
+      arr<<select {|k, v|v==argument}.keys
+    end
+    arr.flatten
   end
 end
+
+hashao={}
+hashao={"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
+#hashao={a: 1, b: 2, c: 3}
+hashao.keys_of("Madagascar","Panama")
